07533f2399849b67a0513822303b1595a08a32b1
Ben Bales <bbbales2@gmail.com>
2016-12-14 16:34:01 -0800

    Updates

--- annotator.py
+++ annotator.py
@@ -10,6 +10,8 @@ pygame.init()
 
 import argparse
 
+from rect import Rect
+
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('video', help = 'Path to video file (preferably .mp4) that contains frames to be annotated')
 parser.add_argument('classFile', help = 'File with newline separated list of class names')
@@ -29,81 +31,70 @@ with open(args.classFile) as f:
 
 F = vid.get_length()
 
-print "Reading in video file..."
-for f in range(F):
-    vid.get_data(f)
-print "Done!"
+#print "Reading in video file..."
+#for f in range(F):
+#    vid.get_data(f)
+#print "Done!"
 
 W, H = vid.get_meta_data()['size']
 
-size = width, height = 256, 256
-speed = [2, 2]
-black = 0, 0, 0
-
-screen = pygame.display.set_mode((W, H))
+screen = pygame.display.set_mode((W + 200, H))
 
 clock = pygame.time.Clock()
 pygame.key.set_repeat(200, 25)
 font = pygame.font.SysFont("monospace", 15)
 
-markers = []
-
-class Marker(object):
-    def __init__(self, f, (x, y)):
-        self.fs = [f]
-        self.xs = [x]
-        self.ys = [y]
-
-    def add(self, f, (x, y)):
-        i = bisect.bisect_left(self.fs, f)
-
-        if i != len(self.fs) and f == self.fs[i]:
-            self.xs[i] = x
-            self.ys[i] = y
-        else:
-            self.fs.insert(i, f)
-            self.xs.insert(i, x)
-            self.ys.insert(i, y)
-
-    def delete(self, f):
-        i = bisect.bisect_left(self.fs, f)
-
-        if i != len(self.fs) and f == self.fs[i]:
-            self.fs.pop(i)
-            self.xs.pop(i)
-            self.ys.pop(i)
-
-            return True
-        else:
-            return False
-
-    def sample(self, f):
-        x = int(numpy.round(numpy.interp([f], self.fs, self.xs)))
-        y = int(numpy.round(numpy.interp([f], self.fs, self.ys)))
-
-        return (x, y), f in self.fs
+rects = []
 
 selected = None
 
 f = 0
+
+state = None
+resize = False
+#'creating'
+ann = {}
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                if selected:
-                    if f not in marker.fs:
-                        print "Adding keyframe to currently selected marker"
-                    else:
-                        print "Moving keyframe in currently selected marker"
-
-                    selected.add(f, event.pos)
+        if event.type == pygame.MOUSEMOTION:
+            if resize == True:
+                if selected and selected.keyframe(f):
+                    selected.setSecondCorner(f, event.pos)
                 else:
-                    print "Adding new keyframe"
-                    markers.append(Marker(f, event.pos))
+                    resize = False
 
-                    selected = markers[-1]
-            elif event.button == 3:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if not selected:
+                for rect in rects:
+                    if rect.contains(f, event.pos):
+                        selected = rect
+                        break
+
+                if not selected:
+                    print "Adding new keyframe"
+                    selected = Rect(f, event.pos, (1, 1))
+
+                    rects.append(selected)
+                    
+                    resize = True
+            else:
+                if not selected.keyframe(f):
+                    print "Adding keyframe to currently selected marker"
+
+                    selected.add(f, event.pos, (1, 1))
+                else:
+                    print "Moving keyframe in currently selected marker"
+
+                    selected.move(f, event.pos)
+
+                resize = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if resize == True:
+                resize = False
+
+            if event.button == 3:
                 dr = None
 
                 x_, y_ = event.pos
@@ -123,7 +114,7 @@ while 1:
                 f = max(0, (f - 1))
 
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_ESCAPE:
+            if event.key in [pygame.K_ESCAPE, pygame.K_RETURN]:
                 selected = None
             elif event.key == pygame.K_d:
                 if selected:
@@ -154,9 +145,32 @@ while 1:
 
     clock.tick(50)
 
-    screen.fill(black)
-    
-    screen.blit(pygame.surfarray.make_surface(numpy.rollaxis(vid.get_data(f), 1, 0)).convert(), (0, 0))
+    screen.fill((0, 0, 0))
+
+    surf = pygame.surfarray.make_surface(numpy.rollaxis(vid.get_data(f), 1, 0))
+
+    for rect in rects:
+        if rect.interpolated(f):
+            color = [255, 0, 0]
+        else:
+            continue
+
+        (x, y), (w, h), keyframe = rect.sample(f)
+
+        x = int(x)
+        y = int(y)
+        w = int(w)
+        h = int(h)
+
+        if keyframe:
+            color = [0, 255, 0]
+
+        if rect == selected:
+            color[2] = 255
+
+        pygame.draw.rect(surf, color, ((x, y), (w, h)), 4)
+
+    screen.blit(surf.convert(), (0, 0))
 
     #for marker in markers:
     #    (x, y), isRed = marker.sample(f)
@@ -174,8 +188,8 @@ while 1:
     #
     #    screen.blit(dotcolor, (x - 10, y - 10))
 
-    label = font.render("{0} / {1}".format(f, F), 1, (255, 255, 255))
+    label = font.render("Frames: {0} / {1}".format(f, F), 1, (255, 255, 255))
 
-    screen.blit(label, (0, 240))
+    screen.blit(label, (W, 0))
 
     pygame.display.flip()
---
+++ rect.py
@@ -0,0 +1,99 @@
+import numpy
+import bisect
+
+class Rect(object):
+    def __init__(self, f, (x, y), (w, h)):
+        self.fs = [f]
+        self.xs = [x]
+        self.ys = [y]
+        self.ws = [w]
+        self.hs = [h]
+        self.resize = False
+
+    def add(self, f, (x, y), (w, h)):
+        i = bisect.bisect_left(self.fs, f)
+
+        if i != len(self.fs) and f == self.fs[i]:
+            self.xs[i] = x
+            self.ys[i] = y
+        else:
+            self.fs.insert(i, f)
+            self.xs.insert(i, x)
+            self.ys.insert(i, y)
+            self.ws.insert(i, w)
+            self.hs.insert(i, h)
+
+    def keyframe(self, f):
+        i = bisect.bisect_left(self.fs, f)
+
+        if i != len(self.fs) and f == self.fs[i]:
+            return True
+
+        return False
+
+    def interpolated(self, f):
+        if f >= self.fs[0] and f <= self.fs[-1]:
+            return True
+        else:
+            return False
+
+    def contains(self, f, (x, y)):
+        if self.interpolated(f):
+            (x_, y_), (w, h), keyframe = self.sample(f)
+
+            if (x_ <= x and x <= x_ + w) and (y_ <= y and y <= y_ + h):
+                return True
+
+        return False
+
+    def move(self, f, (x, y)):
+        i = bisect.bisect_left(self.fs, f)
+
+        self.xs[i] = x
+        self.ys[i] = y
+
+    def setSecondCorner(self, f, (x, y)):
+        i = bisect.bisect_left(self.fs, f)
+
+        print (x, y)
+
+        if x <= self.xs[i]:
+            self.ws[i] = self.xs[i] + self.ws[i] - x
+            self.xs[i] = x
+        elif x <= self.xs[i] + self.ws[i]:
+            self.ws[i] = self.ws[i] - (x - self.xs[i])
+            self.xs[i] = x
+        else:
+            self.ws[i] = x - self.xs[i]
+
+        if y <= self.ys[i]:
+            self.hs[i] = self.ys[i] + self.hs[i] - y
+            self.ys[i] = y
+        elif y <= self.ys[i] + self.hs[i]:
+            self.hs[i] = self.hs[i] - (y - self.ys[i])
+            self.ys[i] = y
+        else:
+            self.hs[i] = y - self.ys[i]
+
+    def delete(self, f):
+        i = bisect.bisect_left(self.fs, f)
+
+        if i != len(self.fs) and f == self.fs[i]:
+            self.fs.pop(i)
+            self.xs.pop(i)
+            self.ys.pop(i)
+            self.ws.pop(i)
+            self.hs.pop(i)
+
+            return True
+        else:
+            return False
+
+    def sample(self, f):
+        x = int(numpy.round(numpy.interp([f], self.fs, self.xs)))
+        y = int(numpy.round(numpy.interp([f], self.fs, self.ys)))
+        w = int(numpy.round(numpy.interp([f], self.fs, self.ws)))
+        h = int(numpy.round(numpy.interp([f], self.fs, self.hs)))
+
+        return (x, y), (w, h), f in self.fs
+
