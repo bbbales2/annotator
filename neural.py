632795c21b73033a7234c5af7118c334110cf5ab
Ben <bbbales2@gmail.com>
2016-12-16 22:02:57 -0800

    Updates

--- neural.py
+++ neural.py
@@ -1,21 +1,31 @@
-            elif event.key == pygame.K_t and ctrl_pressed:
+import pygame
+import sklearn.linear_model
 
+class Network(object):
+    def __init__(self, target):
+        self.classes = {}
+
+    def handle(self, event, g):
+        ctrl_pressed = pygame.key.get_mods() & (pygame.KMOD_RCTRL | pygame.KMOD_LCTRL)
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_t and ctrl_pressed:
                 msg = "Training network..."
                 print msg
 
-                classes = {}
+                self.classes = {}
 
                 Xs = []
                 Ys = []
 
-                for rect in rects:
-                    if rect.label not in classes:
-                        classes[rect.label] = len(classes)
+                for rect in g.ann.rects:
+                    if rect.label not in self.classes:
+                        self.classes[rect.label] = len(self.classes)
 
                     for f_ in rect.fs:
-                        im = vid.get_data(f_)
+                        im = g.vid.get_data(f_)
                         
-                        feats = sess.run(target, feed_dict = { tens : im.reshape(1, im.shape[0], im.shape[1], im.shape[2]) })[0]
+                        feats = sess.run(g.target, feed_dict = { tens : im.reshape(1, im.shape[0], im.shape[1], im.shape[2]) })[0]
 
                         (x, y), (w, h), keyframe = rect.sample(f_)
 
@@ -24,7 +34,7 @@
                             j = int(x + w / 2) / 16
 
                             Xs.append(feats[i, j])
-                            Ys.append(classes[rect.label])
+                            Ys.append(self.classes[rect.label])
 
                         #for i in range(H / 16):
                         #    for j in range(W / 16):
@@ -39,21 +49,24 @@
                 msg = "Network trained!"
                 print msg
 
-    if lgr and selected and selected.label in classes:
-        hist = sess.run(target, feed_dict = { tens : frame.reshape(1, frame.shape[0], frame.shape[1], frame.shape[2]) })[0]#numpy.linalg.norm(, axis = 2)
+    def draw(self, g):
+        g.ann.draw()
 
-        hist = lgr.predict_log_proba(hist.reshape((-1, 1024))).reshape((H / 16, W / 16, -1))
+        if lgr and g.ann.selected and g.ann.selected.label in self.classes:
+            hist = g.sess.run(g.target, feed_dict = { tens : frame.reshape(1, frame.shape[0], frame.shape[1], frame.shape[2]) })[0]
 
-        hist = numpy.argmax(hist, axis = 2).astype('float')#hist[:, :, classes[selected.label]]
+            hist = lgr.predict_log_proba(hist.reshape((-1, 1024))).reshape((g.H / 16, g.W / 16, -1))
 
-        hist -= hist.min()
-        hist /= hist.max()
-        hist = plt.cm.jet(hist)[:, :, :3]
-        hist = (hist * 255).astype('uint8')
+            hist = numpy.argmax(hist, axis = 2).astype('float')#hist[:, :, classes[selected.label]]
+
+            hist -= hist.min()
+            hist /= hist.max()
+            hist = plt.cm.jet(hist)[:, :, :3]
+            hist = (hist * 255).astype('uint8')
+            
+            hist = numpy.kron(hist, numpy.ones((16, 16, 1)))
         
-        hist = numpy.kron(hist, numpy.ones((16, 16, 1)))
+            nn = pygame.surfarray.make_surface(numpy.rollaxis(hist, 1, 0))
+            nn.set_alpha(63)
         
-        nn = pygame.surfarray.make_surface(numpy.rollaxis(hist, 1, 0))
-        nn.set_alpha(63)
-        
-        surf.blit(nn, (0, 0))
+            g.surf.blit(nn, (0, 0))
